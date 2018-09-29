7b64cf3d026e75e2f8cb7173f339de7e19cd5372
majed187 <majedshrndan45@gmail.com>
2018-09-29 12:00:10 +0200

    change some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import MainApp from "./componentes/App";
+import "./styles/main.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <MainApp />
+      </div>
+    );
   }
 }
 
